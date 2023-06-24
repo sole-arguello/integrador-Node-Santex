b3860c36eb2b016384b20855152c07b719b0907d
Arguello Soledad <soledad.ar1@gmail.com>
2023-06-24 20:39:52 -0300

    defino la accion de crear el book que media entre el modeloy el servicio

---
+++ src/providers/providerBook.js
@@ -0,0 +1,14 @@
+const { Book } = require('../models')
+
+
+const createBook = async (book) => {
+    try{
+        const newBook = await Book.create(book)
+        return newBook
+    }catch(err){
+        console.error('Error when creating Book', err)
+        throw err
+    }
+}
+
+module.exports = { createBook }
