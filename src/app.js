06c4e1bf457121bab5ee9f941c75de48a62487c1
Arguello Soledad <soledad.ar1@gmail.com>
2023-06-24 20:46:06 -0300

    defino el middleware, y las rutas

--- src/app.js
+++ src/app.js
@@ -5,6 +5,16 @@ const PORT = 8080
 //exportaciones
 const { initializeDB } = require('./config/db-config')
 
+//middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(`Call made to resource ${req.url} whith method ${req.method}`)
+    next()
+})
+
+//rutas
+app.use('/book', bookRouter)
 
 //puerto escuchando
 app.listen(PORT, async () => {
