a4edbd2a4821a10ae1c4cf8458cf45b88cb81d3e
Arguello Soledad <soledad.ar1@gmail.com>
2023-06-25 16:59:38 -0300

    Agrego rutas para delete, update, get isbn

--- src/routes/routeBook.js
+++ src/routes/routeBook.js
@@ -3,9 +3,12 @@ const router = express.Router()
 const { bookController } = require("../controllers")
 
 router.post('/', bookController.createBook)
-router.get('/', bookController.getAllBook)
+
+router.get('/all', bookController.getAllBook)
+router.get('/:isbnBook', bookController.getIsbnBook)
 router.get('/:bookId', bookController.getBook)
 router.put('/:bookId', bookController.updateBook)
+router.delete('/:bookId', bookController.deleteBook)
 
  
 module.exports = router 
