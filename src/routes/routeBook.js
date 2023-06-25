57d2d4649e98c4ecb23ff0cbd082ba3e67575893
Arguello Soledad <soledad.ar1@gmail.com>
2023-06-25 02:01:38 -0300

    defino rutas para buscar 1 y todos los book

--- src/routes/routeBook.js
+++ src/routes/routeBook.js
@@ -3,5 +3,9 @@ const router = express.Router()
 const { bookController } = require("../controllers")
 
 router.post('/', bookController.createBook)
+router.get('/', bookController.getAllBook)
+router.get('/:bookId', bookController.getBook)
+router.put('/:bookId', bookController.updateBook)
+
  
 module.exports = router 
