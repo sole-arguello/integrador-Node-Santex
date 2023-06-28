36a5c5fcfeaf24ef7043bb4bbe7d0323f39307fc
Arguello Soledad <soledad.ar1@gmail.com>
2023-06-28 14:40:57 -0300

    Modifico db a false y hacer pruebas

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -9,7 +9,7 @@ const initializeDB = async () => {
     try{
         await sequelize.authenticate()
         console.log('Conetion has been established successfully')
-        await sequelize.sync({force: true})
+        await sequelize.sync({force: false})
     }catch(error) {
         console.error('Unable to connect to the database:', error)
     }
