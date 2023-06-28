ce006af95f1fbab33ff32ba8ae4551e58e0a7943
Arguello Soledad <soledad.ar1@gmail.com>
2023-06-28 02:52:02 -0300

    agrego rutas a library y user

--- src/app.js
+++ src/app.js
@@ -4,10 +4,11 @@ const PORT = 8080
 
 //exportaciones
 const { initializeDB } = require('./config/db-config')
-const { bookRouter } = require('./routes')
+const { bookRouter, libraryRouter, userRouter } = require('./routes')
 
 //middleware
 app.use(express.json())
+
 
 app.use((req, res, next) => {
     console.log(`Call made to resource ${req.url} whith method ${req.method}`)
@@ -16,6 +17,8 @@ app.use((req, res, next) => {
 
 //rutas
 app.use('/book', bookRouter)
+app.use('/library', libraryRouter)
+app.use('/user', userRouter)
 
 
 
