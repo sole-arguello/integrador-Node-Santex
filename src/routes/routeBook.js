5762337b7f397ed1c153b4a09f89f59d80654d5f
Arguello Soledad <soledad.ar1@gmail.com>
2023-06-28 00:09:32 -0300

    limpio codigo

--- src/routes/routeBook.js
+++ src/routes/routeBook.js
@@ -4,9 +4,9 @@ const { bookController } = require("../controllers")
 
 router.post('/', bookController.createBook)
 
-router.get('/all', bookController.getAllBook)
-router.get('/:isbnBook', bookController.getIsbnBook)
+router.get('/', bookController.getAllBook)
 router.get('/:bookId', bookController.getBook)
+
 router.put('/:bookId', bookController.updateBook)
 router.delete('/:bookId', bookController.deleteBook)
 
