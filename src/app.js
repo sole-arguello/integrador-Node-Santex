798f60114e564ec6e0c34d7fe96e705a7c88102d
Arguello Soledad <soledad.ar1@gmail.com>
2023-06-28 03:00:13 -0300

    agrego ruta a login

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const PORT = 8080
 
 //exportaciones
 const { initializeDB } = require('./config/db-config')
-const { bookRouter, libraryRouter, userRouter } = require('./routes')
+const { bookRouter, libraryRouter, userRouter, authRouter } = require('./routes')
 
 //middleware
 app.use(express.json())
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 app.use('/book', bookRouter)
 app.use('/library', libraryRouter)
 app.use('/user', userRouter)
-
+app.use('/login', authRouter)
 
 
 
