f2cfc4e0ac54b662a9668b1bb2f1687f5662c45b
Arguello Soledad <soledad.ar1@gmail.com>
2023-06-28 14:41:54 -0300

    Modifico variables

--- src/libs/start-config.js
+++ src/libs/start-config.js
@@ -1,9 +1,9 @@
-const Users = require('../models/user')
+const User = require('../models/user')
 
 const createData = async () =>{
     try{
        
-        const userFound = await Users.findAll({
+        const userFound = await User.findAll({
             where: {
               email: "admin@gmail.com",
             },
@@ -11,17 +11,17 @@ const createData = async () =>{
       
           if (userFound.length > 0) return;
       
-          await Users.create({
-            id: 9,
+          await User.create({
+            id: 0,
             firstName: "admin" ,
             lastName: "lala",
             email: "admin@gmail.com",
             password: "admin",
           });
 
-         console.log("Datos de Usuario insertados correctamente en la base de datos.");
+         console.log("Data of the Administrator User correctly inserted in the database.");
     }catch(err){
-        console.error("Error al insertar los datos de roles:", err);
+        console.error("Error when inserting the User data:", err);
     }
 }
 
