b1797142971fd86a3c3d0e7727707fe447c3290a
Arguello Soledad <soledad.ar1@gmail.com>
2023-06-28 21:57:31 -0300

    Implemento los middleware para proteger las rutas

--- src/models/user.js
+++ src/models/user.js
@@ -9,25 +9,37 @@ const User = sequelize.define('Users', {
         autoIncrement: true,
         unique: true,
     },
-    firstName: {
+    user: {
         type: DataTypes.STRING,
         allowNull: false,
-    },
-    lastName: {
-        type: DataTypes.STRING,
-        allowNull: false,
+        // validate: {
+        //     is: /^[a-z]+$/i,
+        //     len: [3],
+        //     msg: "El campo debe ser mayor a 3 caracteres",
+        // },
     },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true,
+        unique: {
+            arg: true,
+            msg: 'El correo ya ha sido registrado'
+        },
         validate: {
-            isEmail: true,
+            isEmail:{
+                args: true,
+                msg: "Por favor, proporciona una dirección de correo electrónico válida",
+            }
         },
     },
     password: {
         type: DataTypes.STRING,
         allowNull: false,
+        // validate: {
+        //     len: [4],
+        //     msg: "El password debe ser minimo 4 caracteres",
+        // },
+
     },
 },{
     paranoid: true,
--- src/routes/routeUser.js
+++ src/routes/routeUser.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const { userController } = require("../controllers")
+const { jwtValidMDW, userIsAdmin } = require('../middleware/authMdw')
 
-router.post('/', userController.createUser)
+router.post('/',jwtValidMDW, userIsAdmin, userController.createUser)
 
 router.get('/', userController.getAllUsers)
 router.get('/:userId', userController.getUser)
 
-router.put('/:userId', userController.updateUser)
-router.delete('/:userId', userController.deleteUser)
+router.put('/:userId', jwtValidMDW, userIsAdmin, userController.updateUser)
+router.delete('/:userId', jwtValidMDW, userIsAdmin, userController.deleteUser)
 
 
 module.exports = router 
