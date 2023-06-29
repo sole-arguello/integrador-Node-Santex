17989a2d854ac3a687714814c07eda3afedcb178
Arguello Soledad <soledad.ar1@gmail.com>
2023-06-28 22:13:46 -0300

    Defino funcion para Crear un usuario admin al iniciar la db

---
+++ src/config/start-config.js
@@ -0,0 +1,28 @@
+const User = require('../models/user')
+
+const createData = async () =>{
+    try{
+       
+        const userFound = await User.findAll({
+            where: {
+              email: "admin@gmail.com",
+            },
+          });
+      
+          if (userFound.length > 0) return;
+      
+          await User.create({
+            id: 0,
+            firstName: "admin" ,
+            lastName: "lala",
+            email: "admin@gmail.com",
+            password: "admin",
+          });
+
+         console.log("Data of the Administrator User correctly inserted in the database.");
+    }catch(err){
+        console.error("Error when inserting the User data:", err);
+    }
+}
+
+module.exports = createData
