6e9e66897b9bf027bf9309d34cf55161f6852e27
Arguello Soledad <soledad.ar1@gmail.com>
2023-06-29 00:58:10 -0300

    Corrijo errores

--- src/routes/routeUser.js
+++ src/routes/routeUser.js
@@ -3,13 +3,13 @@ const router = express.Router()
 const { userController } = require("../controllers")
 const { jwtValidMDW, userIsAdmin } = require('../middleware/authMdw')
 
-router.post('/',jwtValidMDW, userIsAdmin, userController.createUser)
+router.post('/', userIsAdmin, userController.createUser)
 
 router.get('/', userController.getAllUsers)
 router.get('/:userId', userController.getUser)
 
-router.put('/:userId', jwtValidMDW, userIsAdmin, userController.updateUser)
-router.delete('/:userId', jwtValidMDW, userIsAdmin, userController.deleteUser)
+router.put('/:userId', userIsAdmin, userController.updateUser)
+router.delete('/:userId', userIsAdmin, userController.deleteUser)
 
 
 module.exports = router 
