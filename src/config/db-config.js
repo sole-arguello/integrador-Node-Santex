518c285d20ab18827e976c0d9f83edc521b84fe1
Arguello Soledad <soledad.ar1@gmail.com>
2023-06-30 18:17:09 -0300

    Muevo y renombro archivos

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -9,7 +9,7 @@ const initializeDB = async () => {
     try{
         await sequelize.authenticate()
         console.log('Conetion has been established successfully')
-        await sequelize.sync({force: false})
+        await sequelize.sync({force: true})
     }catch(error) {
         console.error('Unable to connect to the database:', error)
     }
